f7175db10d1e11ffbaaa59b2515b8450c8441468
itu-itis23-kayame22 <kayame22@itu.edu.tr>
2024-04-14 23:42:25 +0300

    30

---
+++ question30.py
@@ -0,0 +1,14 @@
+import random
+
+words = []
+
+with open('sowpods.txt', 'r') as f:
+ line = f.readline().strip()
+ words.append(line)
+ while line:
+    line = f.readline().strip()
+    words.append(line)
+
+random_index = random.randint(0, len(words))
+
+print("Random word: ", words[random_index])
